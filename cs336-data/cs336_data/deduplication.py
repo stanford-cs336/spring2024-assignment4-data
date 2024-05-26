5fe8d00e2c075ff0c35f2a70e958d2a23c0f9139
ZitongYang <zitong@berkeley.edu>
2024-05-25 21:28:01 -0700

    submision

--- cs336-data/cs336_data/deduplication.py
+++ cs336-data/cs336_data/deduplication.py
@@ -1,24 +1,21 @@
-import xxhash
+import hashlib
 import os
 from collections import Counter
 
-
-def exact_line_deduplication(
-    input_files: list[os.PathLike], output_directory: os.PathLike
-):
+def exact_line_deduplication(input_files: list[os.PathLike], output_directory: os.PathLike):
     # First pass: count hashed lines across all files
     line_counts = Counter()
     for file_path in input_files:
         with open(file_path) as file:
             content = file.readlines()
-            hashed_content = [xxhash.xxh32(line).hexdigest() for line in content]
+            hashed_content = [hashlib.sha256(line.encode()).hexdigest() for line in content]
             line_counts.update(hashed_content)
 
     # Second pass: write unique lines to output files
     for file_path in input_files:
         with open(file_path) as file:
             content = file.readlines()
-            hashed_content = [xxhash.xxh32(line).hexdigest() for line in content]
+            hashed_content = [hashlib.sha256(line.encode()).hexdigest() for line in content]
             unique_lines = [
                 line for line, hashed in zip(content, hashed_content) if line_counts[hashed] == 1
             ]
